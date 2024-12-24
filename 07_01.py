21a34f45cccc828373be2d41659828b5c5271286
andron-ua <andron.chipka@gmail.com>
2024-12-24 13:07:22 +0200

    Day #7.

---
+++ 07_01.py
@@ -0,0 +1,22 @@
+ans = 0
+puzzle: dict[list[int]] = {}
+with open('07.data') as file:
+    for line in file:
+        target, nums = line.strip().split(': ')
+        assert int(target) not in puzzle
+        puzzle[int(target)] = [int(_) for _ in nums.split()]
+
+
+def dfs(target: int, curr: int, idx: int, nums: list[int]) -> bool:
+    if curr > target:
+        return False
+    if idx == len(nums):
+        return curr == target
+    return dfs(target, curr + nums[idx], idx + 1, nums) or \
+        dfs(target, curr * nums[idx], idx + 1, nums)
+
+
+for target, nums in puzzle.items():
+    if dfs(target, nums[0], 1, nums):
+        ans = ans + target
+print(ans)
